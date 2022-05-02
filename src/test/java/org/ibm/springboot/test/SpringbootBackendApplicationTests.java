d9af5f9fb1cbf455f5cfbda08e192ae62d0dc15d
subbareddya <subba9678@gmail.com>
2022-05-02 10:43:07 +0530

    initial commit

---
+++ src/test/java/org/ibm/springboot/test/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ibm.springboot.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SpringbootBackendApplicationTests.class)
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
