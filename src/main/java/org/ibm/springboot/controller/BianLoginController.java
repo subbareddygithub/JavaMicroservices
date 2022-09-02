fbf46e38e727374c5927deb5b26a490b6baa7e24
subbareddya <subba9678@gmail.com>
2022-09-02 13:32:09 +0530

    update cors

--- src/main/java/org/ibm/springboot/controller/BianLoginController.java
+++ src/main/java/org/ibm/springboot/controller/BianLoginController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://169.51.207.96:32194")
 @RestController
 @RequestMapping("/api/v1")
 public class BianLoginController {
