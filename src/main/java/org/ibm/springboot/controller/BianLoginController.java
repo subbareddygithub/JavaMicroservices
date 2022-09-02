9c8720167a46349d55eb649048fa1e3f6116de64
subbareddya <subba9678@gmail.com>
2022-09-02 12:42:33 +0530

    Bian changes

---
+++ src/main/java/org/ibm/springboot/controller/BianLoginController.java
@@ -0,0 +1,24 @@
+package org.ibm.springboot.controller;
+
+import org.ibm.springboot.model.BianAccountOpeningRequest;
+import org.ibm.springboot.model.BianLoginRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
+public class BianLoginController {
+	
+	@PostMapping("/login")
+	public String validateLoginDetails(@RequestBody BianLoginRequest bianLoginRequest) {
+		System.out.println("Validate Customer " + bianLoginRequest);
+
+		return "true";
+	}
+
+}
